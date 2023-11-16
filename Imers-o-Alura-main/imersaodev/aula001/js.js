816ddd611abc146bd8e91c0b4f5517a1630ba847
LuksFP <149438171+LuksFP@users.noreply.github.com>
2023-11-16 16:54:21 -0300

    Delete Imers-o-Alura-main/imersaodev directory

--- Imers-o-Alura-main/imersaodev/aula001/js.js
+++
@@ -1,9 +0,0 @@
-
-
-var valorDollar = prompt('Quantos dollares você quer converter')
-var cotacaoDoDollar = 4.85;
-var valorEmReal = valorDollar * cotacaoDoDollar
-valorEmReal = valorEmReal.toFixed(2);
-alert('R$'+ valorEmReal);
-var valorEuro = prompt('Quantos euros você quer converter')
-
